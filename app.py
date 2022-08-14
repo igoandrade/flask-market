8da085551af66990e251be0cb148bac67082c19d
Igo da Costa Andrade <igoandrade.314@gmail.com>
2022-08-14 01:18:10 +0000

    Atualização dos templates em html

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return "<h1>Index</h1>"
+    return render_template("index.html")
+
+@app.route('/market')
+def market_page():
+    return render_template("market.html")
 
 if __name__=="__main__":
     app.run(debug=True)
